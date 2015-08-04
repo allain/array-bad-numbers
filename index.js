039c88a99997ff913ebfa4949ebb1769de45fe61
Mauricio Soares <m.soares2603@gmail.com>
2015-08-04 14:16:37 -0300

    add UMD to badNumbers

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var badNumbers = function(array, badNumbers) {
-  return array.reduce(function(newArray, index) {
-    if(badNumbers.indexOf(index) === -1 && newArray.indexOf(index) === -1) newArray.push(index);
-    return newArray;
-  }, []);
-};
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return (root['badNumbers'] = factory());
+    });
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root['badNumbers'] = factory();
+  }
+}(this, function () {
 
-module.exports = badNumbers;
+	function badNumbers(array, badArray) {
+	  return array.reduce(function(newArray, index) {
+	    if(badArray.indexOf(index) === -1 && newArray.indexOf(index) === -1) newArray.push(index);
+	    return newArray;
+	  }, []);
+	}
+
+	return badNumbers;
+}));
